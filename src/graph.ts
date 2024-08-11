be5a708944bc7660ef96e39c64d427f29a318393
taha.driss <taha.driss3@gmail.com>
2024-08-11 17:26:11 +0200

    graph  + module classes

---
+++ src/graph.ts
@@ -0,0 +1,30 @@
+import { Module } from './module'
+
+export class Graph {
+   modules: Map<string, Module> = new Map()
+
+   constructor(public entryPointPath: string) {
+      // load entryPoint
+   }
+
+   async build() {
+      const module = await this.loadModule(this.entryPointPath)
+      this.addModule(this.entryPointPath, module)
+   }
+
+   async loadModule(path: string): Promise<Module> {
+      const code = await this.loadCode(path)
+      const module = new Module(this, code)
+
+      return module
+   }
+
+   async loadCode(path: string): Promise<string> {
+      const file = Bun.file(path)
+      return file.text()
+   }
+
+   addModule(path: string, module: Module) {
+      this.modules.set(path, module)
+   }
+}
---
+++ src/module.ts
@@ -0,0 +1,47 @@
+import { parse, type Program } from 'acorn'
+import type { Graph } from './graph'
+
+export class Module {
+   graph: Graph
+   ast: Program
+   code: string
+   dependencies: string[] = []
+   imports: Map<string, string> = new Map()
+
+   constructor(graph: Graph, code: string) {
+      this.graph = graph
+      this.code = code
+      this.ast = this.parse(code)
+   }
+
+   parse(content: string): Program {
+      return parse(content, { sourceType: 'module', ecmaVersion: 2020 })
+   }
+
+   async linkDependencies() {
+      this.extractDependencies()
+      for (const dep of this.dependencies) {
+         const depModule = await this.graph.loadModule(dep)
+         this
+      }
+   }
+
+   private extractDependencies() {
+      this.ast.body.forEach((node) => {
+         if (node.type === 'ImportDeclaration') {
+            const source = node.source.value
+            if (source != null && typeof source === 'string') {
+               this.dependencies.push(source)
+            }
+            node.specifiers.forEach((specifier) => {
+               if (specifier.type === 'ImportSpecifier' && specifier.imported.type === 'Identifier') {
+                  const imported = specifier.imported.name
+                  const local = specifier.local.name
+
+                  this.imports.set(local, imported)
+               }
+            })
+         }
+      })
+   }
+}
